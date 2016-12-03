bdd13b7c0c284a9eca69b071b50351fc1914ba7c
ancadiana <ancadianavicol@gmail.com>
2016-12-03 02:57:53 +0200

    mic test

---
+++ Assets/Scripts/MicTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class MicTest : MonoBehaviour {
+
+	int lastSample;
+	AudioClip c;
+	int FREQUENCY = 44100;
+
+
+	void Start () 
+	{
+		c = Microphone.Start(null, true, 100, FREQUENCY);
+		while(Microphone.GetPosition(null) < 0) {} // HACK from Riro
+	}
+
+	void Update()
+	{
+		int pos = Microphone.GetPosition(null);
+		int diff = pos-lastSample;
+		if (diff > 0) 
+		{
+			float[] samples = new float[diff * c.channels];
+			c.GetData(samples, lastSample);
+		}
+		lastSample = pos;
+	}
+}
