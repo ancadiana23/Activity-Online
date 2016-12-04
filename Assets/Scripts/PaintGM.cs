40223f24e8deb2c5e5e51ef8aa30106495b9f7d9
Petra C <petruta.chersunaru@gmail.com>
2016-12-04 22:57:14 +0200

    whiteboard
    whiteboard partially implemented; mouse position bug

---
+++ Assets/Scripts/PaintGM.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaintGM : MonoBehaviour {
+
+    public Canvas canvas;
+    public Transform baseDot;
+    public KeyCode mouseLeft;
+    public static string toolType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (Input.GetKey (mouseLeft))
+        {
+            Transform newObject = (Transform) Instantiate(baseDot, objectPosition, baseDot.rotation);
+            newObject.transform.SetParent(canvas.transform, false);
+            
+        }
+	}
+}
