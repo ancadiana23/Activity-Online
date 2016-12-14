5d868e7963a0ef8e34b063c26f9baf4b262bd40b
Petra C <petruta.chersunaru@gmail.com>
2016-12-15 00:55:02 +0200

    added eraser for whiteboard

---
+++ Assets/Scripts/ToolControlBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolControlBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown ()
+    {
+        //paintGM.toolType = "eraser";
+       // Debug.Log("eraser selected");
+    }
+}
