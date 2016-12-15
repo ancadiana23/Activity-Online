342cd93f0365e6d83cdb87b138bf452b47e5b6c6
ancadiana <ancadianavicol@gmail.com>
2016-12-15 02:52:21 +0200

    gameSession

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour 
+{
+	Team[] teams;
+	int currentTeam;
+	int numTeams;
+
+	// Use this for initialization
+	void Start () 
+	{
+		numTeams = 2;
+		teams = new Team[2];
+		teams[0] = new Team ("Team 1", new Int32[] { 1, 3 });
+		teams[1] = new Team ("Team 2", new Int32[] { 2, 4 });
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void newRound()
+	{
+		currentTeam = (currentTeam + 1) % numTeams;
+		// TODO change dimond
+		// TODO change player name
+
+	}
+
+}
---
+++ Assets/Scripts/Team.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team
+{
+	int[] players;
+	int score;
+	int currentPlayer;
+	string name;
+
+	public Team(string name, int[] players)
+	{
+		this.name = name;
+		this.players = players;
+		this.level = 0;
+		current_player = 0;
+	}
+
+	public int CurrentPlayer
+	{
+		get { return players [currentPlayer];}
+	}
+
+	public int Score
+	{
+		get { return score; }
+		set { score = value; }
+	}
+}
