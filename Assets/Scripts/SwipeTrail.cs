8e20cd5435b34eaa6c44491b133af9b80b306914
Diana-Elena Belmega <diana.belmega@gmail.com>
2017-01-19 01:53:08 +0200

    Integrated the drawing tool
    Note: the Boardgame scene doesn't have a background anymore -> needs
    investigating to figure out how to use the trail renderers with
    backgrounds

--- Assets/Scripts/SwipeTrail.cs
+++ Assets/Scripts/SwipeTrail.cs
@@ -20,7 +20,6 @@ public class SwipeTrail : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("new line");
             thisTrail = Instantiate(trailPrefab) as GameObject;
             thisTrail.transform.parent = GameObject.Find("Canvas").transform;
             mat = thisTrail.GetComponent<TrailRenderer>().material;
@@ -28,8 +27,6 @@ public class SwipeTrail : MonoBehaviour
 
             if (thisTrail == null)
                 Debug.Log("e null");
-            else
-                Debug.Log("perfect");
 
             thisTrail.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -40,7 +37,6 @@ public class SwipeTrail : MonoBehaviour
         }
         else if (Input.GetMouseButton(0))
         {
-            Debug.Log("continue line");
             Ray nRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (!thisTrail)
                 Debug.Log("whaat");
@@ -52,8 +48,6 @@ public class SwipeTrail : MonoBehaviour
         {
             if (thisTrail == null)
                 Debug.Log("e null 2");
-            else
-                Debug.Log("perfect 2");
 
             if (Vector3.Distance(thisTrail.transform.position, startPos) < 0.1)
             {
